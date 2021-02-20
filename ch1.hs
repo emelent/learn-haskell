948d6086c953b411541895951ba74d9978c1e22b
Roy <roy.motloutsi@gmail.com>
2021-02-20 22:29:49 +0200

    Complete chapter 1 of learn.hfm.io

--- ch1.hs
+++ ch1.hs
@@ -12,7 +12,18 @@ inc x = x + 1
 
 conanO'Brien = "It's a me, Conan O'Brien!"
 
-average:: Float -> Float -> Float
+-- average:: Float -> Float -> Float
+average:: Fractional a => a-> a -> a
 average a b = (a + b) / 2.0
 
-anotherAverage a b = (a + b) / 2
+
+-- add:: Num a => a -> a -> a
+add:: Int -> Int -> Int
+add a b = a + b
+
+-- add2:: Int -> Int -> Int  
+-- add2:: Num a => a -> a -> a
+add2 a b = a + b
+
+doSomething :: Eq a => a -> a -> Bool
+doSomething a b = a == b
