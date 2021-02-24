71e4b67c7eb925de23c281b6a0813a319199468f
Roy <roy.motloutsi@gmail.com>
2021-02-24 09:44:28 +0200

    Create allSquares definition

--- lhsk/chapter3/recursion.hs
+++ lhsk/chapter3/recursion.hs
@@ -5,3 +5,8 @@ natSum 0 = 0
 natSum n
   | n > 0 = n + natSum (n - 1)
   | otherwise = error "natSum: Input value too small!"
+
+
+allSquares :: Num a=> [a] -> [a]
+allSquares [] = []
+allSquares (x: xs) = x * x : allSquares xs
---
+++ lyah/chapter2/types.hs
@@ -0,0 +1,5 @@
+-- Chapter 2
+--
+
+same:: (Eq a) => a -> a -> Bool
+same x y = x == y
