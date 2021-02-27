0f2046511c7959e4175e673c981371cfde43230a
Roy <roy.motloutsi@gmail.com>
2021-02-27 15:21:25 +0200

    Setup graphics

---
+++ lhsk/chapter4/Fractals.hs
@@ -0,0 +1,20 @@
+module Fractals where
+
+  -- needed to display the picture in the playground
+import Codec.Picture
+
+  -- our line graphics programming interface
+import LineGraphics
+
+
+house :: Path
+house = [(300, 750), (300, 450), (270, 450), (500, 200),
+         (730, 450), (700, 450), (700, 750)]
+
+door :: Path
+door = [(420, 750), (420, 550), (580, 550), (580, 750)]
+
+
+tryItOut = drawPicture 2.0 [(lightgreen, house), (red, door)]
+
+render fn = writePng "output.png" fn
