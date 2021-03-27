4528c8927ffaedbe6e082c6acdc159883ab6f1e7
Roy <roy.motloutsi@gmail.com>
2021-03-27 15:52:53 +0200

    Gonna end the type stuff here, the rest is just too excessive, don't even know if there's a use case

---
+++ lyah/chapter8/TypeFoo.hs
@@ -0,0 +1,12 @@
+module TypeFoo
+where
+
+import Data.Map
+class Tofu t where
+    tofu:: j a -> t a j
+
+
+data Frank a b = Frank {frankField :: b a } deriving (Show)
+
+instance Tofu Frank where
+    tofu x = Frank x
